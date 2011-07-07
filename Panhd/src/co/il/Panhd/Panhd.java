866c5f1765060443c5aa16472f347923f64e3660
aviadpi@gmail.com <aviadpi@gmail.com@c09053f0-cb85-d288-dd08-60b0f46cc78e>
2011-07-07 02:14:16 +0000

    Added: 1) Gmail and Facebook buttons in Info Dialog 2) Menu and Preferences

--- Panhd/src/co/il/Panhd/Panhd.java
+++ Panhd/src/co/il/Panhd/Panhd.java
@@ -1,85 +1,200 @@
 package co.il.Panhd;
 
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.app.Dialog;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.DialogInterface;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.View.OnClickListener;
+import android.view.Window;
 import android.view.WindowManager;
+import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.FrameLayout;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 public class Panhd extends Activity {
-    /** Called when the activity is first created. */
+
 	int TAKE_PICTURE = 235;
-	int SPLASH_TIME = 2000;
 	private Uri outputFileUri;
-	
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);
-    	
-    // ** Screen Splash
-    	setContentView(R.layout.splash);
-        Thread splashThread = new Thread() {
-           @Override
-           public void run() {
-              try {
-                 int waited = 0;
-                 while (waited < SPLASH_TIME) {
-                    sleep(100);
-                    waited += 100;
-                 }
-              } catch (InterruptedException e) {
-                 // do nothing
-              } finally {
-                 finish();
-                 Intent i = new Intent();
-                 i.setClassName("co.il.Panhd",
-                                "co.il.Panhd.splash");
-                 startActivity(i);
-              }
-           }
-        };
-        splashThread.start();
-    // ** End of Splash Screen
-        
-    // ** Starting the Camera, and calling the result with TAKE_PICTURE
-        
-        String fileName = "PanhdPicture.jpg";
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File file = new File(Environment.getExternalStorageDirectory(), fileName);
-        outputFileUri = Uri.fromFile(file);
-		intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		Display display = getWindowManager().getDefaultDisplay(); 
+		int Wpixels = display.getWidth();
+		int Hpixels = display.getHeight();
 		
-		// ENABLE THIS BEFORE RELEASE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		startActivityForResult(intent, TAKE_PICTURE);
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		final boolean CameraBoolean = preferences.getBoolean("startcamera", false);
+		 
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		 // ** Starting the Camera, and calling the result with TAKE_PICTURE
+		Thread camerathread = new Thread(){
+	           @Override
+	           public void run() {
+	        	   String fileName = "PanhdPicture.jpg";
+	        	   Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+	        	   File file = new File(Environment.getExternalStorageDirectory(), fileName);
+	        	   outputFileUri = Uri.fromFile(file);
+	        	   cameraintent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+	        	   startActivityForResult(cameraintent, TAKE_PICTURE);
+	           }
+ 		};
 		
+		if (CameraBoolean){
+    		camerathread.start();
+    	}
 	// ** End of Camera
 		
-    }
-    
-    @Override
+		// ** About Button
+		ImageButton about = (ImageButton) this.findViewById(R.id.mainAboutButton);
+		about.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				try{
+					final Dialog dialog = new Dialog(view.getContext()); 
+					dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+					dialog.setContentView(R.layout.about);
+
+					Button close = (Button) dialog.findViewById(R.id.aboutCloseButton1);
+					close.setOnClickListener(new OnClickListener()
+					{                
+						public void onClick(View v)
+						{
+							dialog.dismiss(); 
+						} 
+					});
+					// Email Button
+					ImageButton email = (ImageButton) dialog.findViewById(R.id.emailbutton);
+					email.setOnClickListener(new OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+							Intent emailIntent = new Intent(android.content.Intent.ACTION_SEND);
+							emailIntent.putExtra(android.content.Intent.EXTRA_EMAIL, new String[]{"Aviadpi@gmail.com"});
+							emailIntent.setType("*/*");
+							startActivity(emailIntent);
+						}
+					});
+					//Facebook Button
+					ImageButton facebook = (ImageButton) dialog.findViewById(R.id.facebookbutton);
+					facebook.setOnClickListener(new OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+							Uri uri = Uri.parse("http://www.facebook.com/aviad.panhi"); 
+							Intent intent = new Intent(Intent.ACTION_VIEW, uri); 
+							startActivity(intent); 
+						}
+					});
+
+					dialog.show(); 
+				}
+				catch (WindowManager.BadTokenException e){
+					e.getCause();
+				}
+				catch (ClassCastException e){
+					e.getCause();
+				}
+				catch (NullPointerException e){
+					e.getCause();
+				}
+				catch (ActivityNotFoundException e){
+					e.getCause();
+				}
+			}});
+		// ** End of About Button
+
+
+		// ** Start the camera Button
+		Button start = (Button) this.findViewById(R.id.mainStartButton);
+		start.setOnClickListener(new OnClickListener() {
+			public void onClick(View view) {
+				String fileName = "PanhdPicture.jpg";
+				Intent cameraintent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+				File file = new File(Environment.getExternalStorageDirectory(), fileName);
+				outputFileUri = Uri.fromFile(file);
+				cameraintent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+				startActivityForResult(cameraintent, TAKE_PICTURE);
+			}});
+		start.setMinHeight(Hpixels/4);
+		start.setMinWidth(Wpixels/2);
+		// ** End of Camera Button
+	}
+
+	// ** Settings
+	@Override
+	protected void onStop(){
+		super.onStop();
+
+		// Change preferences
+		SharedPreferences settings = getSharedPreferences("Prefs", 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putBoolean("booleanParam", true);
+
+		// Save changes
+		editor.commit();       
+
+	}
+	// ** End of Settings
+	
+	// ** Menu
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.mainmenu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle item selection
+		try {
+			switch (item.getItemId()) {
+			
+			case R.id.menupreferences:
+				Intent settingsActivity = new Intent(this.getBaseContext() ,Preferences.class);
+				startActivity(settingsActivity);
+				return true;
+				
+			case R.id.menualbum:
+				Uri uri = Uri.parse("http://www.facebook.com/media/set/?set=a.477553187029.255929.703267029&l=3a2b9f863b"); 
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri); 
+				startActivity(intent); 
+				return true;
+				
+			default:
+				return super.onOptionsItemSelected(item);
+			}
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	// ** End of Menu
+
+	
+	
+	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data){
- 
-		if (requestCode == TAKE_PICTURE){
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		final boolean SendBoolean = preferences.getBoolean("sendpicture", false);
+		
+		if (requestCode == TAKE_PICTURE && SendBoolean){
 			// Email the picture
 			Intent emailIntent = new Intent(Intent.ACTION_SEND);
 			emailIntent.setType("image/jpeg");
@@ -87,21 +202,7 @@ public class Panhd extends Activity {
 			emailIntent.putExtra(Intent.EXTRA_SUBJECT, "New Panh'd!");
 			emailIntent.putExtra(Intent.EXTRA_TEXT, "Hey Aviad, check out my new Panh'd!");
 			emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(outputFileUri.toString()));
-			startActivity(Intent.createChooser(emailIntent, "Send Email"));
+			startActivity(emailIntent);
 		}
 	}
-    
-    static void createAlertDialog(String msg, Context context, String buttonText) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(context);
-		builder.setMessage(msg);
-		builder.setCancelable(true);
-		builder.setNegativeButton(buttonText,
-				new DialogInterface.OnClickListener() {
-					public void onClick(DialogInterface dialog, int id) {
-					}
-				});
-		AlertDialog alert = builder.create();
-		alert.show();
-	}
-    
 }
---
+++ Panhd/src/co/il/Panhd/SplashScreen.java
@@ -0,0 +1,61 @@
+package co.il.Panhd;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+
+public class SplashScreen extends Activity {
+    /** Called when the activity is first created. */
+	int SPLASH_TIME = 2000;
+	
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+    	// Get the preferences
+    	SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		final boolean SplashBoolean = preferences.getBoolean("splashscreen", false);
+    	
+    // ** Screen Splash
+    	setContentView(R.layout.splash);
+    	
+    	if (SplashBoolean == false){
+    		Thread splashThread = new Thread() {
+ 	           @Override
+ 	           public void run() {
+ 	        	   finish();
+ 	        	   Intent i = new Intent();
+ 	        	   i.setClassName("co.il.Panhd",
+ 	        	   "co.il.Panhd.Panhd");
+ 	        	   startActivity(i);
+ 	           }
+ 	        };
+ 	        splashThread.start();
+    	} else {
+    		Thread splashThread = new Thread() {
+    	           @Override
+    	           public void run() {
+    	              try {
+    	                 int waited = 0; 
+    	                 while (waited < SPLASH_TIME) {
+    	                    sleep(100);
+    	                    waited += 100;
+    	                 }
+    	              } catch (InterruptedException e) {
+    	                 // do nothing
+    	              } finally {
+    	                 finish();
+    	                 Intent i = new Intent();
+    	                 i.setClassName("co.il.Panhd",
+    	                                "co.il.Panhd.Panhd");
+    	                 startActivity(i);
+    	              }
+    	           }
+    	        };
+    	        splashThread.start();
+   	}
+    // ** End of Splash Screen
+    }
+}
