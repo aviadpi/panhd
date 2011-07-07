438b5bc260e4881d447e2bfd102c61c26f2f7ba7
aviadpi@gmail.com <aviadpi@gmail.com@c09053f0-cb85-d288-dd08-60b0f46cc78e>
2011-07-07 12:20:21 +0000

    Added instructions

--- Panhd/gen/co/il/Panhd/R.java
+++ Panhd/gen/co/il/Panhd/R.java
@@ -29,19 +29,20 @@ public final class R {
         public static final int transparent2=0x7f02000f;
     }
     public static final class id {
-        public static final int SPLimage1=0x7f07000a;
-        public static final int aboutCloseButton1=0x7f070004;
+        public static final int SPLimage1=0x7f07000b;
+        public static final int aboutCloseButton1=0x7f070005;
+        public static final int aboutInstructions=0x7f070002;
         public static final int aboutTextView1=0x7f070001;
-        public static final int aboutlinearLayout1=0x7f070002;
+        public static final int aboutlinearLayout1=0x7f070003;
         public static final int abouttextView1=0x7f070000;
-        public static final int emailbutton=0x7f070003;
-        public static final int facebookbutton=0x7f070005;
-        public static final int mainAboutButton=0x7f070009;
-        public static final int mainStartButton=0x7f070007;
-        public static final int mainlinearLayout1=0x7f070006;
-        public static final int mainrelativeLayout1=0x7f070008;
-        public static final int menualbum=0x7f07000c;
-        public static final int menupreferences=0x7f07000b;
+        public static final int emailbutton=0x7f070004;
+        public static final int facebookbutton=0x7f070006;
+        public static final int mainAboutButton=0x7f07000a;
+        public static final int mainStartButton=0x7f070008;
+        public static final int mainlinearLayout1=0x7f070007;
+        public static final int mainrelativeLayout1=0x7f070009;
+        public static final int menualbum=0x7f07000d;
+        public static final int menupreferences=0x7f07000c;
     }
     public static final class layout {
         public static final int about=0x7f030000;
@@ -53,6 +54,8 @@ public final class R {
     }
     public static final class string {
         public static final int Panhd=0x7f050002;
+        public static final int aboutInstructions=0x7f050006;
+        public static final int abouttext=0x7f050007;
         public static final int app_name=0x7f050001;
         public static final int camerabutton=0x7f050004;
         public static final int email=0x7f050003;
